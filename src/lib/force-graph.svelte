eca987e9bf6d516223900796c8d1e9dbb1bf25b4
Maciek <msitkowski94@gmail.com>
2022-01-29 14:29:20 +0100

    handle customizing most of the link and node related props

--- src/lib/force-graph.svelte
+++ src/lib/force-graph.svelte
@@ -1,37 +1,161 @@
 <script lang="ts">
-  import { ForceGraph } from '$lib/force-graph'
+  import * as d3 from 'd3'
+  import type { SimulationNodeDatum } from 'd3'
 
-  export let nodes = []
-  export let links = []
-  export let nodeRadius
+  interface Node extends SimulationNodeDatum {
+    id: string
+    group?: string
+  }
 
-  let svg
-  let width
-  let height
+  interface Link {
+    source: string
+    target: string
+  }
+
+  //
+  // PROPS
+  //
+
+  export let nodes: Node[] = []
+  export let links: Link[] = []
+  export let width: number = 800
+  export let height: number = 600
+
+  //
+  // OPTIONS
+  //
+
+  let nodeId: (n: Node) => string = d => d.id // given d in nodes, returns a unique identifier (string)
+  let nodeGroup: (n: Node) => string = d => d.group // given d in nodes, returns an (ordinal) value for color
+  let nodeGroups // an array of ordinal values representing the node groups
+  let nodeTitle: (n: Node, i?: number) => string // given d in nodes, a title string
+  export let nodeRadius = 5
+  export let nodeFill = 'currentColor'
+  export let nodeStroke = '#fff'
+  export let nodeStrokeWidth = 1.5
+  export let nodeStrokeOpacity = 1
+  let nodeStrength
+  let linkSource: (l: Link, i?: number) => string = ({ source }) => source // given d in links, returns a node identifier string
+  let linkTarget: (l: Link, i?: number) => string = ({ target }) => target // given d in links, returns a node identifier string
+  export let linkStroke = '#999'
+  export let linkStrokeOpacity = 0.6
+  export let linkStrokeWidth = 1.5
+  let linkStrength
+  let colors = d3.schemeTableau10 // an array of color strings, for the node groups
+
+  //
+  // COMPUTE VALUES
+  //
+
+  const nodesIds = nodes.map(nodeId)
+  const linksSources = links.map(linkSource)
+  const linksTargets = d3.map(links, linkTarget)
+
+  if (nodeTitle === undefined) nodeTitle = (_, i: number) => nodesIds[i]
+  const nodesTitles = nodeTitle == null ? null : nodes.map(nodeTitle)
+
+  const nodesGroups = nodeGroup == null ? null : nodes.map(nodeGroup)
+
+  // Replace the input nodes and links with mutable objects for the simulation.
+  nodes = nodes.map((_, i) => ({ id: nodesIds[i] }))
+  links = links.map((_, i) => ({ source: linksSources[i], target: linksTargets[i] }))
+
+  // Compute default domains.
+  if (nodesGroups && nodeGroups === undefined) nodeGroups = d3.sort(nodesGroups)
+
+  // Construct the scales.
+  const color = nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors)
+
+  // Construct the forces.
+  const forceNode = d3.forceManyBody()
+  const forceLink = d3.forceLink<Node, Link>(links).id(({ index: i }) => nodesIds[i])
+  if (nodeStrength !== undefined) forceNode.strength(nodeStrength)
+  if (linkStrength !== undefined) forceLink.strength(linkStrength)
+
+  let svgEl
 
   $: {
-    if (svg) {
-      const graph = ForceGraph(svg, { nodes, links}, {
-        nodeId: d => d.id,
-        nodeGroup: d => d.group || 'repo',
-        nodeTitle: d => `${d.id} (${d.group})`,
-        width,
-        height,
-        nodeRadius
-      })
+    if (svgEl) {
+      const svg = d3.select(svgEl)
+
+      const simulation = d3
+        .forceSimulation<Node>(nodes)
+        .force<Node, Link>('link', forceLink)
+        .force<Node, Link>('charge', forceNode)
+        .force<Node, Link>('x', d3.forceX())
+        .force<Node, Link>('y', d3.forceY())
+        .on<Node, Link>('tick', () => ticked(link, node))
+
+      const link = svg.select('g').selectAll<SVGLineElement, Link>('line').data(links).join('line')
+
+      const node = svg
+        .select('g')
+        .selectAll<SVGCircleElement, Node>('circle')
+        .data(nodes)
+        .join('circle')
+        .call(drag(simulation))
+
+      if (nodesTitles) node.append('title').text(({ index: i }) => nodesTitles[i])
     }
+  }
+
+  function ticked(link, node) {
+    link
+      .attr('x1', d => d.source.x)
+      .attr('y1', d => d.source.y)
+      .attr('x2', d => d.target.x)
+      .attr('y2', d => d.target.y)
+
+    node.attr('cx', d => d.x).attr('cy', d => d.y)
+  }
+
+  function drag(simulation) {
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart()
+      event.subject.fx = event.subject.x
+      event.subject.fy = event.subject.y
+    }
+
+    function dragged(event) {
+      event.subject.fx = event.x
+      event.subject.fy = event.y
+    }
+
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0)
+      event.subject.fx = null
+      event.subject.fy = null
+    }
+
+    return d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended)
   }
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
-
-<svg bind:this={svg}>
+<svg bind:this={svgEl} {width} {height} viewBox={[-width / 2, -height / 2, width, height]}>
   <g>
     {#each links as link}
-      <line stroke='#999' />
+      <line
+        stroke={linkStroke}
+        stroke-opacity={linkStrokeOpacity}
+        stroke-width={linkStrokeWidth}
+        stroke-linecap="round"
+      />
     {/each}
-    {#each nodes as node}
-      <circle r="10"  />
+    {#each nodes as node, i}
+      <circle
+        r={nodeRadius}
+        fill={nodeFill}
+        stroke={nodeStroke}
+        stroke-opacity={nodeStrokeOpacity}
+        stroke-width={nodeStrokeWidth}
+      />
     {/each}
   </g>
 </svg>
+
+<style>
+  svg {
+    max-width: 100%;
+    height: auto;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,82 @@
-<script lang="ts">
-  import miserablesData from '$lib/miserables.json'
-  import ForceGraph from '$lib/force-graph.svelte'
+<script>
+  import Graph from '$lib/force-graph.svelte'
+  import miserables from '../miserables.json'
 
-  const { nodes, links } = miserablesData
+  const { nodes, links } = miserables
 
+  let innerWidth = 0
+  let innerHeight = 0
+
+  // Node
   let nodeRadius = 5
+  let nodeFill = '#000'
+  let nodeStroke = '#fff'
+  let nodeStrokeWidth = 1.5
+  let nodeStrokeOpacity = 1
 
+  // Link
+  let linkStroke = '#999'
+  let linkStrokeOpacity = 0.6
+  let linkStrokeWidth = 1.5
+
+  $: options = {
+    width: innerWidth,
+    height: innerHeight,
+
+    nodeRadius,
+    nodeFill,
+    nodeStroke,
+    nodeStrokeWidth,
+    nodeStrokeOpacity,
+
+    linkStroke,
+    linkStrokeOpacity,
+    linkStrokeWidth,
+  }
 </script>
 
-<ForceGraph
-  {nodes} {links} {nodeRadius} />
+<svelte:window bind:innerWidth bind:innerHeight />
+
+<Graph {nodes} {links} {...options} />
 
 <aside>
   <h1>D3 Force-Directed Graph</h1>
   <hr />
+
+  <h2>Node</h2>
   <label>
-    <input type='number' bind:value={nodeRadius}  />
-    Node radius
+    <input type="number" bind:value={nodeRadius} />
+    radius
+  </label>
+  <label>
+    <input type="text" bind:value={nodeFill} maxlength={7} />
+    fill
+  </label>
+  <label>
+    <input type="text" bind:value={nodeStroke} maxlength={7} />
+    stroke
+  </label>
+  <label>
+    <input type="number" bind:value={nodeStrokeOpacity} step={0.05} min={0} max={1} />
+    stroke opacity
+  </label>
+  <label>
+    <input type="number" bind:value={nodeStrokeWidth} step={0.5} min={0} max={10} />
+    stroke width
+  </label>
+
+  <h2>Link</h2>
+  <label>
+    <input type="text" bind:value={linkStroke} maxlength={7} />
+    stroke
+  </label>
+  <label>
+    <input type="number" bind:value={linkStrokeOpacity} step={0.05} min={0} max={1} />
+    stroke opacity
+  </label>
+  <label>
+    <input type="number" bind:value={linkStrokeWidth} step={0.5} min={0} max={10} />
+    stroke width
   </label>
 </aside>
 
@@ -25,11 +85,18 @@
     position: fixed;
     top: 0;
     left: 0;
-    padding: 1rem;
+    padding: 16px;
     height: 100vh;
     width: 300px;
   }
   h1 {
     margin-top: 0;
+  }
+  input {
+    width: 80px;
+  }
+  label {
+    display: block;
+    margin-bottom: 2px;
   }
 </style>
